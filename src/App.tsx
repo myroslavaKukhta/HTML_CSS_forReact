b2205ca69183fa0bf3afe549f46b7021f0eaaffb
Myroslava Kukhta <107687502+myroslavaKukhta@users.noreply.github.com>
2023-10-08 19:01:47 +0300

    Second_Oct8

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,93 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import styled from "styled-components";
 
 function App() {
   return (
     <div className="App">
-        <StyledBtn >Hello </StyledBtn >
+        <Box>
+        <Menu>
+          <ul>
+            <li><a href="">menu item 1</a></li>
+            <li><a href="">menu item 2</a></li>
+            <li><a href="">menu item 3</a></li>
+          </ul>
+        </Menu>
+            <StyledBtn as={Link} href={"#"}>LinkComponent
+            </StyledBtn >
+            <StyledBtn as="a" href={"#"}>Link
+            </StyledBtn >
+            <StyledBtn>hello
+            </StyledBtn >
+            <SuperBtn>Super</SuperBtn>
+        </Box>
+
     </div>
   );
 }
 
 export default App;
+
 const StyledBtn = styled.button`
 border: none;
-  background-color: rebeccapurple;
+  background-color: #06e186;
   padding: 10px 20px;
-  border-radius: 5px;
   color: white;
   font-size: 16px;
+
+  &:hover {
+background-color: #085233;
+  }
+
+  &:last-child {
+    background-color: #f7f30bec;
+  }
+`
+
+const Link = styled.a`
+  color: #96f30b;
+  font-size: 16px;
+  font-weight: bold;
+  background-color: transparent;
+  padding: 0;
+`
+
+const SuperBtn = styled(StyledBtn)`
+  border-radius: 5px;
+  background-color: red;
+  color: greenyellow;
+`
+
+const Box = styled.div `
+display: flex;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+
+  button {
+    cursor: pointer;
+  }
+
+  ${Link} {
+    cursor: zoom-in;
+  }
+`
+
+const Menu = styled.nav `
+ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+}
+
+li > a {
+  color: green;
+}
+
+li + li {
+  margin-left: 20px;
+}
+
 `
